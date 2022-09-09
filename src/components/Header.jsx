98fa0d0a6a584397f8c475a7322b20e207f04386
MousumiMitu <mousumitu@gmail.com>
2022-09-10 00:33:08 +0600

    header working

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="fixed z-50 w-screen bg-slate-500 p-6">
+      {/* desktop and tab */}
+      <div className="hidden md:block w-full h-full  "></div>
+
+      {/* mobile */}
+      <div className="block md:hidden w-full h-full "></div>
+    </div>
+  );
 };
 
 export default Header;
