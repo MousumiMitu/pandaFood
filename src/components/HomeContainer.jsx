26234c990b2fd81d3b48aa49e979bea38f468e95
MousumiMitu <mousumitu@gmail.com>
2022-09-12 23:36:19 +0600

    create container started

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -9,7 +9,7 @@ const HomeContainer = () => {
       className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full "
       id="home"
     >
-      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6 border">
+      <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6 ">
         <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
             Bike delivery
